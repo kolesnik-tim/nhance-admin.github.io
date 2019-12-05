6e12ac10838eef8a2ab2143e8ceb5ea7db1d3568
Tim <ktn_96@icloud.com>
2019-12-05 11:39:29 +0200

    add colors

--- src/js/import/ui.js
+++ src/js/import/ui.js
@@ -13,3 +13,32 @@
 //   });
 //   return false;
 // });
+
+//drop-down open
+$('.drop-down--open').on('click', function(e) {
+  e.preventDefault();
+  $(this).find('ul').fadeIn();
+});
+//document mouseup
+$(document).mouseup(function(e) {
+  var container = $('.admin-header__notifications ul, .admin-header__user ul');
+  if (container.has(e.target).length === 0) {
+    container.fadeOut();
+  }
+});
+
+
+
+//aside-admin
+$('.aside-admin b').click(function() {
+  $(this).toggleClass('active').next().slideToggle();
+});
+
+
+
+//aside-mobile
+$('.menu-open').on('click', function() {
+  $(this).toggleClass('active');
+  $('.aside-admin').toggleClass('active');
+  $('.admin').toggleClass('active');
+});
