3b22a834e1dff3534b7f6a0bae90da2898ef719c
Tim <ktn_96@icloud.com>
2020-07-11 00:44:13 +0300

    fix

--- src/js/import/ui.js
+++ src/js/import/ui.js
@@ -107,9 +107,76 @@ $(document).mouseup(function(e) {
 
 
 //pop-up btn continue
-$('#slide-right-dx .continue').on('click', function() {
+$('#slide-right-dx .continue, #slide-right-dx .skip').on('click', function() {
   if($('#slide-right-dx .tab-content .tab-pane.active').index() !== 3) {
     $('#slide-right-dx .nav-step li a.active').removeClass('active').parent().next().find('a').addClass('active');
     $('#slide-right-dx .tab-content .tab-pane.active').removeClass('active show').next().addClass('active show');
+    $('#slide-right-dx .skip').fadeOut();
   }
+  if($('#slide-right-dx .tab-content .tab-pane.active').index() === 2) {
+    $('#slide-right-dx .skip').fadeIn();
+  }
+  if($('#slide-right-dx .tab-content .tab-pane.active').index() === 3) {
+    $('#slide-right-dx .continue').text('Issue Now');
+    $('#slide-right-dx .skip').fadeOut();
+  }
+});
+$('#slide-right-dx .nav-step li a').click(function() {
+  setTimeout(function() {
+    if($('#slide-right-dx .tab-content .tab-pane.active').index() === 3) {
+      $('#slide-right-dx .continue').text('Issue Now');
+      $('#slide-right-dx .skip').fadeOut();
+    } else{
+      $('#slide-right-dx .continue').text('Continue');
+      $('#slide-right-dx .skip').fadeOut();
+    }
+    if($('#slide-right-dx .tab-content .tab-pane.active').index() === 2) {
+      $('#slide-right-dx .skip').fadeIn();
+    }
+  }, 300);
+});
+
+
+//input file
+$(document).ready( function() {
+  $('body').on('change', '#slide-right-dx .tab-pane .file input', function() {
+    var filename = $(this).val().replace(/.*\\/, '');
+    $(this).parent().next().text(filename);
+  });
+});
+
+
+//Issue DX Card step 3
+$('body').on('click', '#slide-right-dx .tab-pane label', function() {
+  let label = $(this);
+  if(label.attr('data-radio') === 'file') {
+    label.parents('.radio-block').next('.input-block').find('.url').fadeOut(300);
+    setTimeout(function() {
+      label.parents('.radio-block').next('.input-block').find('.file').fadeIn();
+    }, 300);
+    
+  } else if(label.attr('data-radio') === 'url') {
+    label.parents('.radio-block').next('.input-block').find('.file').fadeOut(300);
+    setTimeout(function() {
+      label.parents('.radio-block').next('.input-block').find('.url').fadeIn();
+    }, 300);
+    
+  }
+});
+
+//Issue DX Card delet step 3
+$('body').on('click', '#slide-right-dx .tab-pane .btn-danger', function(event) {
+  event.preventDefault();
+  $(this).parent().remove();
+});
+
+$('#slide-right-dx .tab-pane .add-btn span, #slide-right-dx .tab-pane .add-btn b').on('click', function() {
+  $(this).parents('.add-btn').toggleClass('active');
+});
+
+
+
+$('#slide-right-dx .add-btn .invoice').on('click', function() {
+  
+  $('#slide-right-dx #step-3').append('<div class="block-white m-b-15"><h5 class="fz-14 bold all-caps text-black title-invoice m-b-25">Invoice '+ 0 + (+$('.title-invoice').length+1) +'</h5><div class="form__block m-b-35 flex"><input type="text" id="name-4"><label for="name-4">Name</label><a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Up here!"><svg class="icon icon-information "><use xlink:href="img/sprite.svg#icon-information"></use></svg></a></div><div class="flex-wrap m-b-45 radio-block"><strong class="text-black m-b-10 m-r-20">File Details</strong><div class="form__radio m-r-20"><input type="radio" name="radio-04" id="radio-09" checked=""><label class="text-black fz-14" for="radio-09" data-radio="file">Upload File</label></div><div class="form__radio m-r-20"><input type="radio" name="radio-04" id="radio-10"><label class="text-black fz-14" for="radio-10" data-radio="url">URL</label></div><div class="form__radio"><input type="radio" name="radio-04" id="radio-11"><label class="text-black fz-14" for="radio-11" data-radio="invoice">Generate Invoice</label></div></div><div class="input-block"><div class="form__block m-b-35 flex file"><div><input type="file" id="file" placeholder="Upload File"></div><label for="file">Upload File</label><a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Up here!"><svg class="icon icon-information "><use xlink:href="img/sprite.svg#icon-information"></use></svg></a><svg class="icon icon-clip "><use xlink:href="img/sprite.svg#icon-clip"></use></svg></div><div class="form__block m-b-35 flex url" style="display:none"><input type="url" id="url"><label for="url">URL</label><a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Up here!"><svg class="icon icon-information "><use xlink:href="img/sprite.svg#icon-information"></use></svg></a></div></div><a class="btn btn-danger btn-sm" href="#">delete</a></div>');
 });
