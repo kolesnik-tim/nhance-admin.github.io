67773b5b63f32ea9b3497c9bdc0b2c1b3460389e
Tim <ktn_96@icloud.com>
2020-08-27 15:13:38 +0300

    fix

--- src/js/import/ui.js
+++ src/js/import/ui.js
@@ -6,6 +6,14 @@ import Quill from 'quill';
 import fancyapps from '@fancyapps/fancybox';
 
 
+
+//tabs
+$('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+  $(this).addClass('active').siblings().removeClass('active')
+    .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+});
+
+
 //hide modal
 $('#slide-right-dx').on('hide.bs.modal', function(e) {
   console.log($('#slide-right-dx input').val());
